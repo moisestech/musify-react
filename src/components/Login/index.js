36c0924aab14bbf1883f0d151327e41be1cd70d3
Moises Sanabria <sesmoi.data@gmail.com>
2020-12-18 05:44:33 -0500

    init Login component w/ PropTypes

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,1 +1,21 @@
-index.js
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import { accessUrl } from './spotify'
+
+export default function Login () {
+  return (
+    <div className='login'>
+      <img
+        src=''
+        alt=''
+      />
+      <a href={accessUrl}>
+        LOGIN TO MUSIFY
+      </a>
+    </div>
+  )
+}
+
+Login.propTypes = {
+
+}
