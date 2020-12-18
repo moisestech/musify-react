7b7a9429ea61d63904b16977249a3274e0a2d594
Moises Sanabria <sesmoi.data@gmail.com>
2020-12-18 05:43:34 -0500

    init Body component w/ PropTypes

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,1 +1,19 @@
-index.js
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import Header from './Header'
+import SongRow from './SongRow'
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import MoreHorizonIcon from '@material-ui/icons/MoreHoriz'
+
+export default function Body ({ spotify }) {
+  return (
+    <div classNames='body'>
+
+    </div>
+  )
+}
+
+Body.propTypes = {
+  spotify: PropTypes.string.isRequired
+}
