173f674ee04110e2fd0e7c249c04b8d6bc1608c3
Moises Sanabria <sesmoi.data@gmail.com>
2020-12-18 05:51:41 -0500

    accessUrl authEndpoint getTOkenFromResponse accessUrl

--- src/utils/spotifyAPI.js
+++ src/utils/spotifyAPI.js
@@ -1,1 +1,10 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = 'https://accounts.spotify.com/authorize'
+
+const clientId = ''
+const redirectUri = ''
+const scopes = []
+
+export const getTokenFromResponse = () => null
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`
